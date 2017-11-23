17ad67085392af8868619bef85dbdf435fd2e0d8
Joao Diogo <pedrod.isa1@gmail.com>
2017-11-23 23:56:03 +0000

    added background

--- core/src/org/academiadecodigo/hackathon/apologies/game/screens/TestScreen.java
+++ core/src/org/academiadecodigo/hackathon/apologies/game/screens/TestScreen.java
@@ -2,7 +2,10 @@ package org.academiadecodigo.hackathon.apologies.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.mail.vandrake.VLib;
+import com.mail.vandrake.scene2d.VImage;
 import com.mail.vandrake.scene2d.VLabel;
 import com.mail.vandrake.scene2d.VScreen;
 import org.academiadecodigo.hackathon.apologies.AllApologies;
@@ -10,6 +13,8 @@ import org.academiadecodigo.hackathon.apologies.game.objects.Platform;
 import org.academiadecodigo.hackathon.apologies.game.objects.Player;
 
 public class TestScreen extends VScreen {
+
+    private Image bkgImage;
 
     @Override
     public void show() {
@@ -20,6 +25,9 @@ public class TestScreen extends VScreen {
         Player player = new Player(100, 100, VLib.guiSkin.getRegion("neko_logo"));
         Platform platform = new Platform(300, 300, VLib.guiSkin.getRegion("window"));
 
+        bkgImage = VImage.fromFile(Gdx.files.internal("background1.jpg"));
+
+        getGuiStage().addActor(bkgImage);
         getGuiStage().addActor(player);
         getGuiStage().addActor(platform);
 
