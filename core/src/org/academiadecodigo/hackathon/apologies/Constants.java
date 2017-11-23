68302e86d4714c8d340b90541c3f3892563b3313
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-23 18:05:55 +0000

    added gameObjects

---
+++ core/src/org/academiadecodigo/hackathon/apologies/Constants.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.hackathon.apologies;
+
+/**
+ * Created by codecadet on 23/11/17.
+ */
+public class Constants {
+}
---
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/Platform.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.hackathon.apologies.game.objects;
+
+/**
+ * Created by codecadet on 23/11/17.
+ */
+public class Platform {
+}
