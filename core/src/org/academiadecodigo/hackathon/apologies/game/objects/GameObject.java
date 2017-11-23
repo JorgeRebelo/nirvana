149344fc92630fda3000bf609bbe92d0f1c9cb7e
Barros <guilhermeandre104@gmail.com>
2017-11-23 19:25:39 +0000

    Player class updated

--- core/src/org/academiadecodigo/hackathon/apologies/game/objects/GameObject.java
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/GameObject.java
@@ -1,7 +1,16 @@
 package org.academiadecodigo.hackathon.apologies.game.objects;
 
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mail.vandrake.scene2d.VActor;
+
 /**
  * Created by codecadet on 23/11/17.
  */
-public class GameObject {
+public class GameObject extends VActor {
+
+
+    public GameObject(float x, float y, TextureRegion sprite) {
+        super(x, y, sprite);
+    }
+
 }
--- core/src/org/academiadecodigo/hackathon/apologies/game/objects/Player.java
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/Player.java
@@ -1,4 +1,31 @@
 package org.academiadecodigo.hackathon.apologies.game.objects;
 
-public class Player {
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player extends GameObject{
+
+    //powerUp
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+
+    public Player(float x, float y, TextureRegion sprite) {
+        super(x, y, sprite);
+        position = new Vector2(x, y);
+        velocity = new Vector2(0,0);
+        acceleration = new Vector2(0,0);
+
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+
+    }
+
+
+
 }
