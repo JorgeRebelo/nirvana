d52f202b192cd42574b5c8f4e719f629c53e74e5
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-23 17:05:05 +0000

    Added Library

--- desktop/src/org/academiadecodigo/hackathon/apologies/desktop/DesktopLauncher.java
+++ desktop/src/org/academiadecodigo/hackathon/apologies/desktop/DesktopLauncher.java
@@ -4,10 +4,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import org.academiadecodigo.hackathon.apologies.AllApologies;
 
-public clasgit remote add origin https://github.com/JorgeRebelo/nirvana.git
-        s DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new AllApologies(), config);
-	}
+public class DesktopLauncher {
+
+    public static void main(String[] arg) {
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new AllApologies(), config);
+    }
 }
