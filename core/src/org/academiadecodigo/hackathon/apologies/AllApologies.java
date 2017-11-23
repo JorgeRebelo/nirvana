80ad68006300a5dc2f6788f14fe9407ee896429c
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-23 17:14:16 +0000

    VLib is working

--- core/src/org/academiadecodigo/hackathon/apologies/AllApologies.java
+++ core/src/org/academiadecodigo/hackathon/apologies/AllApologies.java
@@ -1,33 +1,34 @@
 package org.academiadecodigo.hackathon.apologies;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mail.vandrake.VLib;
 
-public class AllApologies extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class AllApologies extends Game {
+    VLib vLib;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        vLib = new VLib();
+
+        vLib.create();
+
+        vLib.start();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    }
+
+    @Override
+    public void dispose() {
+        vLib.dispose();
+    }
 }
--- desktop/src/org/academiadecodigo/hackathon/apologies/desktop/DesktopLauncher.java
+++ desktop/src/org/academiadecodigo/hackathon/apologies/desktop/DesktopLauncher.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.hackathon.apologies.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mail.vandrake.control.GameDefs;
 import org.academiadecodigo.hackathon.apologies.AllApologies;
 
 public class DesktopLauncher {
@@ -9,6 +10,8 @@ public class DesktopLauncher {
     public static void main(String[] arg) {
 
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = GameDefs.PC_WIDTH;
+        config.height = GameDefs.PC_HEIGHT;
         new LwjglApplication(new AllApologies(), config);
     }
 }
