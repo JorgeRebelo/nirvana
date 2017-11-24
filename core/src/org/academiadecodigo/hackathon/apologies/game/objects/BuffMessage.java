318b3e0ace27aa7300373be1046e2cb38f49b943
Barros <guilhermeandre104@gmail.com>
2017-11-24 00:40:43 +0000

    Enums created

---
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/BuffMessage.java
@@ -0,0 +1,16 @@
+package org.academiadecodigo.hackathon.apologies.game.objects;
+
+public enum BuffMessage {
+    LEVEL_1(""),
+    LEVEL_2(""),
+    LEVEL_3(""),
+    LEVEL_4("");
+
+    private String message;
+
+    BuffMessage(String string) {
+
+        message = string;
+
+    }
+}
