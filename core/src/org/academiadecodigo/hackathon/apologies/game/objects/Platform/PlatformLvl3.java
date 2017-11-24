57ef46466b85435eb06fba1905e13f003edc539c
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-24 07:42:06 +0000

    Merged with Ice platform

--- core/src/org/academiadecodigo/hackathon/apologies/game/objects/Platform/PlatformLvl3.java
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/Platform/PlatformLvl3.java
@@ -15,7 +15,7 @@ public class PlatformLvl3 extends GameObject {
 
         super(x, y, sprite);
 
-        body = BodyFactory.polygonShape(world, (int) x, (int) y, 2f, 0.6f, BodyDef.BodyType.StaticBody, 1);
+        body = BodyFactory.polygonShape(world, (int) x, (int) y, 2f, 0.6f, BodyDef.BodyType.StaticBody, 40);
         body.setFixedRotation(true);
     }
 
