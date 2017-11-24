c73d324f0e0c7a863644148881fcc2d5356630c4
Jorge Rebelo <jorge.rebelo.ae@gmail.com>
2017-11-24 07:37:32 +0000

    Added animations

--- core/src/org/academiadecodigo/hackathon/apologies/game/objects/Player.java
+++ core/src/org/academiadecodigo/hackathon/apologies/game/objects/Player.java
@@ -3,12 +3,13 @@ package org.academiadecodigo.hackathon.apologies.game.objects;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mail.vandrake.control.VSound;
+import org.academiadecodigo.hackathon.apologies.AllApologies;
 import org.academiadecodigo.hackathon.apologies.SoundManager;
+import org.academiadecodigo.hackathon.apologies.game.screens.GameScreen;
 import org.academiadecodigo.hackathon.apologies.utils.Constants;
 
 import static org.academiadecodigo.hackathon.apologies.game.objects.AnimationFactory.*;
@@ -19,27 +20,29 @@ public class Player extends GameObject {
     private long lastPlayed = 0;
     private int lives = 3;
     private TextureRegion playerImage;
-    private Animation<TextureRegion> walkRight[] = new Animation[2];
-    private Animation<TextureRegion> walkLeft[] = new Animation[2];
+    private Animation<TextureRegion> walkRight[] = new Animation[5];
+    private Animation<TextureRegion> walkLeft[] = new Animation[5];
 
     //Constructor
     public Player(float x, float y, World world) {
 
-        super(x, y, textureRegion("player_lvl1_L.png"));
-        playerImage = textureRegion("player_lvl4_R.png");
+        super(x, y, textureRegion("player_1_L1.png"));
+        playerImage = textureRegion("player_4_R1.png");
 
         body = BodyFactory.polygonShape(world, (int) x, (int) y, 0.65f, 1f, BodyDef.BodyType.DynamicBody);
         body.setFixedRotation(true);
 
-        for (int i = 1; i < 2; i++) {
+        for (int level = 1; level < 5; level++) {
 
             //TODO replace this
-            String playerLevel = "player_lvl";
-            walkRight[i] = new Animation<TextureRegion>(Constants.ANIMATION_FRAME_TIME, textureRegion(playerLevel + i + "_L.png"));
-            walkRight[i].setPlayMode(Animation.PlayMode.LOOP);
+            String playerLevel = "player_";
+            walkRight[level] = new Animation<TextureRegion>(Constants.ANIMATION_FRAME_TIME, textureRegion(playerLevel + level + "_L1.png"),
+                    textureRegion(playerLevel + level + "_L2.png"));
+            walkRight[level].setPlayMode(Animation.PlayMode.LOOP);
 
-            walkLeft[i] = new Animation<TextureRegion>(Constants.ANIMATION_FRAME_TIME, textureRegion(playerLevel + i + "_R.png"));
-            walkLeft[i].setPlayMode(Animation.PlayMode.LOOP);
+            walkLeft[level] = new Animation<TextureRegion>(Constants.ANIMATION_FRAME_TIME, textureRegion(playerLevel + level + "_R1.png"),
+                    textureRegion(playerLevel + level + "_R2.png"));
+            walkLeft[level].setPlayMode(Animation.PlayMode.LOOP);
         }
     }
 
@@ -66,6 +69,10 @@ public class Player extends GameObject {
         super.act(delta);
 
         curTime += delta;
+        if (body.getLinearVelocity().x == 0) {
+
+            curTime = 0;
+        }
 
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) || (Gdx.input.isKeyPressed(Input.Keys.W))) {
 
